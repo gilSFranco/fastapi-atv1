2e819c7d72e1581077deb2dbe1d7f038be047d17
DSM6 <fatec-dsm6@academico.intra>
2025-10-23 20:01:02 -0300

    feat: first commit

---
+++ app/main.py
@@ -0,0 +1,43 @@
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+from typing import List
+
+app = FastAPI()
+
+
+class Estudante(BaseModel):
+    id: int
+    nome: str
+    email: str
+
+
+class CriarEstudante(BaseModel):
+    nome: str
+    email: str
+
+
+lista_estudantes: List[Estudante] = [
+    Estudante(id=1, nome="Gilmar", email="gilmar@gmail.com")
+]
+
+
+@app.get("/estudantes", response_model=List[Estudante])
+def listar_estudantes():
+    return lista_estudantes
+
+
+@app.get("/estudantes/{id_estudante}", response_model=Estudante)
+def listar_estudante_por_id(id_estudante: int):
+    for estudante in lista_estudantes:
+        if estudante.id == id_estudante:
+            return estudante
+    raise HTTPException(status_code=404, detail="Estudante não encontrado.")
+
+
+@app.post("/estudantes", status_code=201, response_model=Estudante)
+def criar_estudante(estudante: CriarEstudante):
+    id_gerado = max([estudante.id for estudante in lista_estudantes], default=0) + 1
+
+    novo_estudante = Estudante(id=id_gerado, nome=estudante.nome, email=estudante.email)
+    lista_estudantes.append(novo_estudante)
+    return novo_estudante
---
+++ tests/test_main.py
@@ -0,0 +1,40 @@
+from fastapi.testclient import TestClient
+from app.main import app
+
+client = TestClient(app)
+
+def testando_listar_estudante_sucesso():
+    response = client.get("/estudantes")
+    assert response.status_code == 200
+    assert response.json() == [
+        {"id": 1, "nome": "Gilmar", "email": "gilmar@gmail.com"}
+    ]
+
+
+def testando_listar_estudante_por_id_sucesso():
+    id_estudante = 1
+    response = client.get(f"/estudantes/{id_estudante}")
+    assert response.status_code == 200
+    assert response.json() == {"id": 1, "nome": "Gilmar", "email": "gilmar@gmail.com"}
+
+
+def testando_listar_estudante_por_id_falha():
+    id_estudante = 2
+    response = client.get(f"/estudantes/{id_estudante}")
+    assert response.status_code == 404
+    assert response.json() == {"detail": "Estudante não encontrado."}
+
+
+def testando_criar_estudante_sucesso():
+    response = client.post(
+        "/estudantes", json={"nome": "Victor", "email": "victor@gmail.com"}
+    )
+    assert response.status_code == 201
+    assert response.json() == {"id": 2, "nome": "Victor", "email": "victor@gmail.com"}
+
+
+def testando_criar_estudante_falha():
+    response = client.post(
+        "/estudantes", json={"email": "lucas@gmail.com"}
+    )
+    assert response.status_code == 422
